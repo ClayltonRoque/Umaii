fcb3d3b954079e9289e94b10cceb673b62d30313
claylton <claylton202@gmail.com>
2025-08-10 23:18:15 -0300

    criado logica de authenticação com o devise

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem "kamal", require: false
 gem "thruster", require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
+gem "devise"
+gem "devise-jwt"
 # gem "image_processing", "~> 1.2"
 
 group :development, :test do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
   validates :name, presence: { message: "não pode ficar em branco" }
   validates :user_type, presence: { message: "é obrigatório informar o tipo de usuário" }
   has_many :orders
